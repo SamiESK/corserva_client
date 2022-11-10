810c6c1ad6efeb04a637a75b75f4f1fc8111ebf0
Sami Eskirjeh <shomee@live.com>
2022-11-09 20:44:37 -0500

    Integrated Prettier and ESLint for syntax structure.

--- src/components/CreateSale.component.js
+++ src/components/CreateSale.component.js
@@ -1,38 +1,39 @@
-
 // CreateSale Component for add new sale
-  
+
 // Import Modules
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
-import SaleForm from "./SaleForm";
-  
+import SaleForm from './SaleForm';
+
 // CreateSale Component
 const CreateSale = () => {
-  const [formValues] = 
-    useState({ productName: '', description: '', price: '' })
-  // onSubmit handler
-  const onSubmit = saleObject => {
-    axios.post(
-    'http://localhost:3001/sales', 
-    saleObject)
-      .then(res => {
-        if (res.status === 201)
-          alert('Sale successfully created')
-        else
-          Promise.reject()
-      })
-      .catch(err => alert('Something went wrong'))
-  }
-    
-  // Return sale form
-  return(
-    <SaleForm initialValues={formValues} 
-      onSubmit={onSubmit} 
-      enableReinitialize>
-      Create Sale!
-    </SaleForm>
-  )
-}
-  
+    const [formValues] = useState({
+        productName: '',
+        description: '',
+        price: '',
+    });
+    // onSubmit handler
+    const onSubmit = (saleObject) => {
+        axios
+            .post('http://localhost:3001/sales', saleObject)
+            .then((res) => {
+                if (res.status === 201) alert('Sale successfully created');
+                else Promise.reject();
+            })
+            .catch((err) => alert('Something went wrong'));
+    };
+
+    // Return sale form
+    return (
+        <SaleForm
+            initialValues={formValues}
+            onSubmit={onSubmit}
+            enableReinitialize
+        >
+            Create Sale!
+        </SaleForm>
+    );
+};
+
 // Export CreateSale Component
 export default CreateSale;
--- src/components/SalesList.component.js
+++ src/components/SalesList.component.js
@@ -1,44 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+import SaleTableRow from './SaleTableRow.component';
 
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Table } from "react-bootstrap";
-import SaleTableRow from "./SaleTableRow.component";
-  
 const SaleList = () => {
-  const [sales, setSales] = useState([]);
-  
-  useEffect(() => {
-    axios
-      .get("http://localhost:3001/sales/")
-      .then(({ data }) => {
-        setSales(data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  
-  const DataTable = () => {
-    return sales.map((res, i) => {
-      return <SaleTableRow obj={res} key={i} />;
-    });
-  };
-  
-  return (
-    <div className="table-wrapper">
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Product Name</th>
-            <th>Description</th>
-            <th>USD $</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>{DataTable()}</tbody>
-      </Table>
-    </div>
-  );
+    const [sales, setSales] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/sales/')
+            .then(({ data }) => {
+                setSales(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const DataTable = () =>
+        sales.map((res, i) => <SaleTableRow obj={res} key={i} />);
+    return (
+        <div className="table-wrapper">
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Description</th>
+                        <th>USD $</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>{DataTable()}</tbody>
+            </Table>
+        </div>
+    );
 };
-  
+
 export default SaleList;
